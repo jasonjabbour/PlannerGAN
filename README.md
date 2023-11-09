523d44e9901c5ed30149809c80a3a4c2aaf14d8e
jasonjabbour <jjj4se@virginia.edu>
2023-11-09 00:51:12 -0500

    sim setup

--- README.md
+++ README.md
@@ -31,3 +31,18 @@ source /tmp/PlannerGAN_ws/install/setup.bash
 # Launch the Gazebo Simulation
 ros2 launch iiwa_simulation iiwa_gazebo.launch.py
 ```
+
+
+Second Attempt
+```shell
+
+vcs import src < src/iiwa_ros2/iiwa_ros2.repos
+rosdep install --ignore-src --from-paths . -y -r
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
+source install/setup.bash
+
+source /usr/share/gazebo/setup.sh
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/tmp/PlannerGAN_ws/src/iiwa_ros2
+ros2 launch iiwa_bringup iiwa.launch.py use_sim:="true"
+
+```
--- iiwa_simulation/launch/iiwa_gazebo.launch.py
+++ iiwa_simulation/launch/iiwa_gazebo.launch.py
@@ -1,67 +1,33 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import ThisLaunchFileDir
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    # Define the path to the empty Gazebo world file
-    empty_world_path = PathJoinSubstitution([
-        FindPackageShare('gazebo_ros'),
-        'worlds',
-        'empty.world'
-    ])
-    
-    # Define the path to the iiwa XACRO file
-    iiwa_xacro_path = PathJoinSubstitution([
-        FindPackageShare("iiwa_description"),
-        'urdf',
-        'iiwa.urdf.xacro'
-    ])
-    
-    # Command to convert XACRO to URDF
-    urdf_content = Command(['xacro ', iiwa_xacro_path])
+    # Get the path to the iiwa_description package
+    iiwa_description_pkg_path = get_package_share_directory('iiwa_description')
 
-    # Gazebo server launch
-    gazebo_server_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzserver.launch.py'
-            ])
-        ]),
-        launch_arguments={'world': empty_world_path}.items(),
-    )
+    # Set the path to the iiwa URDF/Xacro file
+    iiwa_urdf_file = os.path.join(iiwa_description_pkg_path, 'urdf', 'iiwa.urdf.xacro')
 
-    # Gazebo client launch
-    gazebo_client_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzclient.launch.py'
-            ])
-        ])
-    )
+    # Set the path to the Gazebo launch file
+    gazebo_launch_file = os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
 
-    # Robot state publisher node
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='both',
-        parameters=[{'robot_description': urdf_content}],
-    )
-
-    # Define launch description
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'world',
-            default_value=empty_world_path,
-            description='The Gazebo world to load'
+        # Include Gazebo launch
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([gazebo_launch_file]),
+            launch_arguments={'world': os.path.join(iiwa_description_pkg_path, 'gazebo', 'worlds', 'empty.world')}.items(),
         ),
-        gazebo_server_launch,
-        gazebo_client_launch,
-        robot_state_publisher
+
+        # Spawn iiwa URDF/Xacro
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-entity', 'iiwa', '-topic', 'robot_description'],
+            output='screen',
+        ),
     ])
